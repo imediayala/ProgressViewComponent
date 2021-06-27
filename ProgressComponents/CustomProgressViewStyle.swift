cc4d598a02630f256f9c74979e8bba707862d730
Daniel Ayala Jabon <imediayala@gmail.com>
2021-06-27 11:05:22 +0200

    first commit

---
+++ ProgressComponents/CustomProgressViewStyle.swift
@@ -0,0 +1,20 @@
+//
+//  CustomProgressViewStyle.swift
+//  ProgressComponents
+//
+//  Created by Daniel Ayala on 25/6/21.
+//
+
+import SwiftUI
+
+struct CustomProgressViewStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomProgressViewStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomProgressViewStyle()
+    }
+}
---
+++ ProgressComponents/ProgressViewSampleView.swift
@@ -0,0 +1,20 @@
+//
+//  ProgressVieweSampleView.swift
+//  ProgressComponents
+//
+//  Created by Daniel Ayala on 27/6/21.
+//
+
+import SwiftUI
+
+struct ProgressVieweSampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgressVieweSampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressVieweSampleView()
+    }
+}
